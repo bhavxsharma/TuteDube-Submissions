1469e5a9a2fa454dff67e21555d1e50e39ee13de
bhavxsharma <78663339+bhavxsharma@users.noreply.github.com>
2025-06-19 16:49:38 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+#print(max(1,2,3,4,5))
+#print(min(2,12,4,15,23,45,3))
+#print(abs(-23))
+#print(pow(2,3))
+
+a=input("Enter First Number: ")
+a=int(a)
+b=input("Enter Second Number ")
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition:',c)
+print('Subtraction:',d)
+print('Multiplication:',e)
+print('Division:',f)
---
+++ Task 2.py
@@ -0,0 +1,3 @@
+a=input("Enter Your First Name : ")
+b=input("Enter Your Last Name: ")
+print('Hello,' ,a,b +'!', 'Welcome to your Python Program!')
